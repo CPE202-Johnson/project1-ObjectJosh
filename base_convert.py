eeea47a4179f5915ba297585ab813dc184f7e157
ObjectJosh <joshwong74@gmail.com>
2021-01-14 10:45:23 -0800

    Completed fixed convert function

--- base_convert.py
+++ base_convert.py
@@ -6,10 +6,12 @@ def convert(num, b):
     Returns:
         str: string representation of num in the base b
     """
+    letter_list = ["A", "B", "C", "D", "E", "F", "G"]
     if num // b == 0:
+        if num % b >= 10:
+            return str(letter_list[num % b - 10]) # convert to alpha
         return str(num)
     base = num % b
-    letter_list = ["A", "B", "C", "D", "E", "F", "G"]
     if num % b >= 10:
-        base = letter_list[num % b - 10]
+        base = letter_list[num % b - 10] # convert to alpha
     return str(convert(num // b, b)) + str(base)
