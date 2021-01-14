9cf5a76857d89398304c051f8de4783a8e133ea5
ObjectJosh <joshwong74@gmail.com>
2021-01-14 10:56:24 -0800

    Completed deleted extra name == main func

--- base_convert.py
+++ base_convert.py
@@ -15,8 +15,3 @@ def convert(num, b):
     if num % b >= 10:
         base = letter_list[num % b - 10] # convert to alpha
     return str(convert(num // b, b)) + str(base)
-
-if __name__ == "__main__":
-    print(convert(16, 16))
-    print(convert(160, 16))
-    print(convert(0, 2))
